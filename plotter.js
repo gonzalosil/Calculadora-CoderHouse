aa5efe17c1b4f355a7fdc0eb884bcd74e4ddc8cf
Gonzalo Silva <30927470+gonzalosil@users.noreply.github.com>
2022-10-30 22:24:20 -0300

    Terminado

--- plotter.js
+++ plotter.js
@@ -12,7 +12,9 @@ class Function{
     x = [];
     y = [];
     data = [];
+    id;
     constructor(func, id){
+        this.func = func;
         for (let x = 0; x <= 10; x += 0.4){
             this.x.push(x);
             this.y.push(eval(func))
@@ -31,7 +33,7 @@ function new_function(event) {
     
     // actualizarUsuarioStorage();
     // mostrarTextoUsuario();
-    draw_plot(id);
+    draw_plot();
     id++;
   }
 
@@ -44,18 +46,88 @@ function initialize_events(){
     add_function.onsubmit = (event) => new_function(event);
 }
 
-function draw_plot(id){
+function draw_plot(){
 //layout.push( = {title: "[y=" + exp1 + "] [y=" + exp2 + "] [y=" + exp3 + "]"});
-for (i = 0; i<Functions.length; i++){
-    all_data[i] = (Functions[i].data);
-}
-Plotly.newPlot("myPlot", all_data);
+    for (i = 0; i<Functions.length; i++){
+        all_data[i] = (Functions[i].data);
+    }
+    Plotly.newPlot("myPlot", all_data);
+    update_func_storage();
+    draw_funcs();
 }
 
+function draw_funcs() {
+    func_list.innerHTML = "";
+    Functions.forEach((calc, index) => {
+      let column = document.createElement("div");
+        column.className = "";
+        column.id = `row-${calc.id}`;
+        column.innerHTML = `
+              <div class="card">
+                  <div class="card-body">
+                  <li class="card-text">Trace (${index}): Y =
+                      <b>${calc.func}</b>
+                  </li>
+                  </div>
+                  <div class="card-footer">
+                      <button class="delete_btn" id="botonEliminar-${calc.id}" ><p>Eliminar</p></button>
+                  </div>
+              </div>`;
+  
+      func_list.append(column);
+  
+      let botonEliminar = document.getElementById(`botonEliminar-${calc.id}`);
+      botonEliminar.onclick = () => confirm_item_delete(calc.id);
+    });
+}
+
+
+function confirm_item_delete(idProducto) {
+    Swal.fire({
+      icon: "question",
+      title: "¿Estas seguro que quieres eliminar el producto?",
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        delete_Calculation(idProducto);
+      }
+    });
+  }
+
+  function delete_Calculation(id_Calc) {
+    let index = 0;
+    // let columnaBorrar = document.getElementById(`row-${id_Calc}`);
+    for (let i = 0; i < Functions.length ; i++){
+        if(Functions[i].id == id_Calc){
+            index = i;
+            break;
+        }
+    }
+    Functions.splice(index, 1);
+    Plotly.deleteTraces("myPlot", 0);
+    draw_funcs();
+    draw_plot();
+  }
+
+  function update_func_storage() {
+    let FunctionsJSON = JSON.stringify(Functions);
+    localStorage.setItem("Functions", FunctionsJSON);
+  }
+
+  function get_func_storage() {
+    let FunctionsJSON = localStorage.getItem("Functions");
+    if (FunctionsJSON){
+        Functions =  (JSON.parse(FunctionsJSON));
+        draw_plot();
+    }
+  }
 
 function  main(){
     initialize_elements();
     initialize_events();
+    get_func_storage();
 }
 
 main();
